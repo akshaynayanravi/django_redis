63f7a062c0196857f59b82ae7b4ffa415ca0cc05
akshaynayanravi <orakshaykumar@gmail.com>
2022-01-25 12:58:25 +0530

    Created a model for app1

---
+++ app1/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Tasks(models.Model):
+    title = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    completed_at = models.DateTimeField()
+    deadline = models.DateTimeField()
+    tags = models.ManyToManyField(Tag)
+
+    class TaskStatus(models.TextChoices):
+        COMPLETED = "CO", "Completed"
+        PENDING = "PE", "Pending"
+        DROPPED = "DR", "Dropped"
+    status = models.CharField(
+        choices=TaskStatus.choices,
+        default=TaskStatus.PENDING,
+        max_length=2
+    )
--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 def home(request):
-    return render(request, 'home.html', {'title': 'Web Calculator'})
+    return render(request, 'home.html', {'title': 'Web Calculator', 'datetime_now': str(datetime.now())})
 
 
 def add_numbers_page(request):
@@ -10,8 +11,8 @@ def add_numbers_page(request):
 
 
 def add_numbers(request):
-    num_1 = int(request.GET.get('num1'))
-    num_2 = int(request.GET.get('num2'))
+    num_1 = int(request.POST.get('num1'))
+    num_2 = int(request.POST.get('num2'))
     result = num_1 + num_2
 
     return render(request, 'result.html', {'result': result})
